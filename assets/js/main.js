453126f7565572a4bc0b80c2931637aa11253f37
alip55247 <alip55247@gmail.com>
2021-10-26 15:11:24 +0700

    Update Main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-/*===== MENU SHOW =====*/ 
+/* MENU SHOW */ 
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -11,7 +11,7 @@ const showMenu = (toggleId, navId) =>{
 }
 showMenu('nav-toggle','nav-menu')
 
-/*===== ACTIVE AND REMOVE MENU =====*/
+/* ACTIVE AND REMOVE MENU */
 const navLink = document.querySelectorAll('.nav__link');   
 
 function linkAction(){
@@ -19,21 +19,20 @@ function linkAction(){
   navLink.forEach(n => n.classList.remove('active'));
   this.classList.add('active');
   
-  /*Remove menu mobile*/
+  /* Remove menu mobile */
   const navMenu = document.getElementById('nav-menu')
   navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/*===== SCROLL REVEAL ANIMATION =====*/
+/* SCROLL REVEAL ANIMATION */
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
     duration: 2000,
-    reset: true
 });
 
-/*SCROLL HOME*/
+/* SCROLL HOME */
 sr.reveal('.home__title',{}); 
 sr.reveal('.button',{delay: 200}); 
 sr.reveal('.home__img',{delay: 400}); 
